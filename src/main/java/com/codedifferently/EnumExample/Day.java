1b540326bccb990f42df79cb531625ebada479d7
Student 24 <student24@codes-air-73.myfiosgateway.com>
2023-09-24 17:28:04 -0400

    finished enum example

---
+++ src/main/java/com/codedifferently/EnumExample/Day.java
@@ -0,0 +1,11 @@
+package com.codedifferently.EnumExample;
+
+public enum Day {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
--- src/main/java/com/codedifferently/EnumExample/Main.java
+++ src/main/java/com/codedifferently/EnumExample/Main.java
@@ -1,20 +1,57 @@
 package com.codedifferently.EnumExample;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        //DeckOfCards card =  DeckOfCards.HEARTS;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter Day of the Week");
+        String userInput = scanner.nextLine().toUpperCase();
+
+        Day today = null;
+
+        try {
+            today = Day.valueOf(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Tomorrow is a New Day");
+        }
+
+        if (today != null) {
+            System.out.println("Today is " + today);
+            switch (today) {
+
+                case SUNDAY:
+                    System.out.println("Brace ahead for the week");
+                    break;
+                case MONDAY:
+                    System.out.println("Case of the Mondays. /:");
+                    break;
+                case TUESDAY:
+                    System.out.println("Taco Tuesday!");
+                    break;
+                case WEDNESDAY:
+                    System.out.println("Hump Day");
+                case THURSDAY:
+                    System.out.println("Friday is around");
+                    break;
+                case FRIDAY:
+                    System.out.println("Thank God it's Friday!");
+                    break;
+                case SATURDAY:
+                    System.out.println("Today Was a Good Day");
+                    break;
+            }
+
+
+//        System.out.println("All days of the week:");
+//        for (Day day : Day.values()) {
+//            System.out.println(day);
+        }
+        scanner.close();
     }
 
-
-
-
 }
-
-//CLUB, DIAMOND, SPADE, HEART;
-//Enum = enumerated (ordered listing of items in a collection that are constant)
-
-
-
 
